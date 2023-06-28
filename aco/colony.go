0d089a21193f5f5fff3a87e9dc2339adc0d54329
Lucas <lucastarling1@gmail.com>
2023-06-27 21:38:48 -0300

    New weighted random choice

--- aco/colony.go
+++ aco/colony.go
@@ -3,151 +3,160 @@ package aco
 import (
 	"math"
 	"math/rand"
+	"sort"
 
 	set "github.com/hashicorp/go-set"
+	"gonum.org/v1/gonum/floats"
 )
 
 type colony struct {
-    ants int
-    generations int
-    minPheromone float64
-    maxPheromone float64
-    evaporationRate float64
-    bestClique []string
-    graph Graph
+	ants            int
+	generations     int
+	minPheromone    float64
+	maxPheromone    float64
+	evaporationRate float64
+	bestClique      []string
+	graph           Graph
 }
 
 func NewColony(ants, iterations int, minPheromone, maxPheromone, evaporationRate float64, graph map[string]map[string]float64) *colony {
-    return &colony{
-        ants: ants,
-        generations: iterations,
-        minPheromone: minPheromone,
-        maxPheromone: maxPheromone,
-        evaporationRate: evaporationRate,
-        graph: graph,
-    }
+	return &colony{
+		ants:            ants,
+		generations:     iterations,
+		minPheromone:    minPheromone,
+		maxPheromone:    maxPheromone,
+		evaporationRate: evaporationRate,
+		graph:           graph,
+	}
 }
 
 // Run returns the max clique and the stats for the run
 func (c colony) Run() (int, [][]int) {
-    var mean, worst int
-    stats := make([][]int, c.generations)
-    for i := 0; i < c.generations; i++ {
-        cliques := make([][]string, c.ants)
-        for ant := 0; ant < c.ants; ant++ {
-            clique := c.buildClique()
-            cliques = append(cliques, clique)
-        }
-        c.updatePheromones(cliques)
-        mean, worst = c.getStats(cliques)
-        stats[i] = []int{len(c.bestClique), worst, mean}
-    }
-    return len(c.bestClique), stats
+	var mean, worst int
+	stats := make([][]int, c.generations)
+	for i := 0; i < c.generations; i++ {
+		cliques := make([][]string, c.ants)
+		for ant := 0; ant < c.ants; ant++ {
+			clique := c.buildClique()
+			cliques = append(cliques, clique)
+		}
+		c.updatePheromones(cliques)
+		mean, worst = c.getStats(cliques)
+		stats[i] = []int{len(c.bestClique), worst, mean}
+	}
+	return len(c.bestClique), stats
 }
 
 func (c colony) getStats(cliques [][]string) (int, int) {
-    worst := c.bestClique 
-    total := 0
-    for _, clique := range cliques {
-        total += len(clique)
-        if len(clique) < len(worst) {
-            worst = clique
-        }
-    }
-    mean := total / len(cliques)
-    return mean, len(worst)
+	worst := c.bestClique
+	total := 0
+	for _, clique := range cliques {
+		total += len(clique)
+		if len(clique) < len(worst) {
+			worst = clique
+		}
+	}
+	mean := total / len(cliques)
+	return mean, len(worst)
 }
 
 // updatePheromones uses elitism (best solution is used to update the pheromones)
 func (c *colony) updatePheromones(cliques [][]string) {
-    bestClique := []string{}
-    for _, clique := range cliques {
-        if len(clique) > len(bestClique) {
-            bestClique = clique
-        }
-    }
+	bestClique := []string{}
+	for _, clique := range cliques {
+		if len(clique) > len(bestClique) {
+			bestClique = clique
+		}
+	}
 
-    if len(bestClique) > len(c.bestClique) {
-        c.bestClique = bestClique
-    }
-    // Evaporate pheromone on all edges
-    for k1, vertices := range c.graph {
-        for k2, pheromone := range vertices {
-            newVal := pheromone - (pheromone * c.evaporationRate)
-            if newVal < c.minPheromone {
-                newVal = c.minPheromone
-            }
-            c.graph[k1][k2] = newVal
-        }
-    }
-    // Deposit pheromones for best ant
-    bestDiff := float64(len(c.bestClique) - len(bestClique))
-    for _, vertex1 := range bestClique {
-        for _, vertex2 := range bestClique {
-            if vertex1 == vertex2 {
-                continue
-            }
-            newVal := c.graph[vertex1][vertex2] + (1 / (1 + bestDiff))
-            if newVal > c.maxPheromone {
-                newVal = c.maxPheromone
-            }
-            c.graph[vertex1][vertex2] = newVal
-        }
-    }
+	if len(bestClique) > len(c.bestClique) {
+		c.bestClique = bestClique
+	}
+	// Evaporate pheromone on all edges
+	for k1, vertices := range c.graph {
+		for k2, pheromone := range vertices {
+			newVal := pheromone - (pheromone * c.evaporationRate)
+			if newVal < c.minPheromone {
+				newVal = c.minPheromone
+			}
+			c.graph[k1][k2] = newVal
+		}
+	}
+	// Deposit pheromones for best ant
+	bestDiff := float64(len(c.bestClique) - len(bestClique))
+	for _, vertex1 := range bestClique {
+		for _, vertex2 := range bestClique {
+			if vertex1 == vertex2 {
+				continue
+			}
+			newVal := c.graph[vertex1][vertex2] + (1 / (1 + bestDiff))
+			if newVal > c.maxPheromone {
+				newVal = c.maxPheromone
+			}
+			c.graph[vertex1][vertex2] = newVal
+		}
+	}
 }
 
 func (c colony) buildClique() []string {
-    initialVertex := c.graph.GetRandomKey()
-    clique := set.From([]string{initialVertex})
-    candidates := set.From(c.graph.GetNeighbours(initialVertex))
-    for {
-        if candidates.Empty() {
-            break
-        }
-        candStr := candidates.Slice()
-        cliqueStr := clique.Slice()
-        pheromoneFactors := c.getPheromoneFactors(candStr, cliqueStr)
-        nextVertex := getRandomNode(candStr, pheromoneFactors)
-        clique.Insert(nextVertex)
-        nextNeighbours := set.From(c.graph.GetNeighbours(nextVertex))
-        candidates = candidates.Intersect(nextNeighbours)
-    }
-    return clique.Slice() 
-}
-
-// TODO: I believe this random pick from a slice can be improved
-func getRandomNode(nodes []string, factors []float64) string {
-    probSum, pheromProbs := getChoiceProbability(factors)
-    val := rand.Float64() * probSum
-    for idx := range nodes {
-        val -= pheromProbs[idx]
-        if val <= 0 {
-            return nodes[idx]
-        }
-    }
-    return nodes[len(nodes)-1]
+	initialVertex := c.graph.GetRandomKey()
+	clique := set.From([]string{initialVertex})
+	candidates := set.From(c.graph.GetNeighbours(initialVertex))
+	for {
+		if candidates.Empty() {
+			break
+		}
+		candStr := candidates.Slice()
+		cliqueStr := clique.Slice()
+		pheromoneFactors := c.getPheromoneFactors(candStr, cliqueStr)
+		nextVertex := getRandomNode(candStr, pheromoneFactors)
+		clique.Insert(nextVertex)
+		nextNeighbours := set.From(c.graph.GetNeighbours(nextVertex))
+		candidates = candidates.Intersect(nextNeighbours)
+	}
+	return clique.Slice()
 }
 
 func (c colony) getPheromoneFactors(vertices, clique []string) []float64 {
-    factors := []float64{}
-    for _, vertex := range vertices {
-        sum := 0.0
-        for _, cvert := range clique {
-            sum += c.graph[vertex][cvert]
-        }
-        factors = append(factors, math.Pow(sum, 2))
-    }
-    return factors
+	factors := []float64{}
+	for _, vertex := range vertices {
+		sum := 0.0
+		for _, cvert := range clique {
+			sum += c.graph[vertex][cvert]
+		}
+		factors = append(factors, math.Pow(sum, 2))
+	}
+	return factors
 }
 
 func getChoiceProbability(factors []float64) (float64, []float64) {
-    probabilities := []float64{}
-    sum := 0.0
-    for _, factor := range factors {
-        sum += factor
-    }
-    for _, factor := range factors {
-        probabilities = append(probabilities, factor / sum)
-    }
-    return sum, probabilities
+	probabilities := []float64{}
+	sum := 0.0
+	for _, factor := range factors {
+		sum += factor
+	}
+	for _, factor := range factors {
+		probabilities = append(probabilities, factor/sum)
+	}
+	return sum, probabilities
+}
+
+func weightedChoice(nodes []string, weights []float64) string {
+	cdf := make([]float64, len(weights))
+	floats.CumSum(cdf, weights)
+	val := rand.Float64() * cdf[len(cdf)-1]
+	idx := sort.Search(len(cdf), func(i int) bool { return cdf[i] > val })
+	return nodes[idx]
+}
+
+func getRandomNode(nodes []string, factors []float64) string {
+	probSum, pheromProbs := getChoiceProbability(factors)
+	val := rand.Float64() * probSum
+	for idx := range nodes {
+		val -= pheromProbs[idx]
+		if val <= 0 {
+			return nodes[idx]
+		}
+	}
+	return nodes[len(nodes)-1]
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/franciscobonand/aoc-max-clique
 
 go 1.20
 
-require github.com/hashicorp/go-set v0.1.13
+require (
+	github.com/hashicorp/go-set v0.1.13
+	gonum.org/v1/gonum v0.13.0
+)
+
+require golang.org/x/exp v0.0.0-20230626212559-97b1e661b5df // indirect
